d03ea7b918821d8400cee3c53a366a96504dd265
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-06-24 14:48:30 +0200

    Create Multiples of 3 or 5.cpp

---
+++ Multiples of 3 or 5.cpp
@@ -0,0 +1,13 @@
+int solution(int number) 
+{
+  if(number < 0)
+    return 0;
+  else
+  {
+    int sum = 0;
+    for(int i = 0; i < number; ++i)
+      if(i % 3 == 0 || i % 5 == 0)
+        sum += i;
+    return sum;
+  }
+}
